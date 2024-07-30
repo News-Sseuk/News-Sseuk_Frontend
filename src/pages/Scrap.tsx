b46739be0755e09852e3cd58a576b5f7069352c5
yujinsim <yuooo26@naver.com>
2024-07-30 15:40:19 +0900

    modiftying scrap page

--- src/pages/Scrap.tsx
+++ src/pages/Scrap.tsx
@@ -1,11 +1,31 @@
 import NavigationBar from "../components/NavigationBar";
 import styled from "styled-components";
+import ArticleCard from "../components/ArticleCard";
+
 const Scrap = () => {
   return (
     <>
       <Div>
-        <h1>Scrap</h1>
-        <NavigationBar />
+        <Header>
+          <CategoryWrapper>
+            <Img>이미지</Img>
+            <CategoryName>정치</CategoryName>
+          </CategoryWrapper>
+        </Header>
+        <ContentWrapper>
+          <Title>내 스크랩</Title>
+          <ArticleContainer>
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+          </ArticleContainer>
+        </ContentWrapper>
+        <NavigationBarWrapper>
+          <NavigationBar />
+        </NavigationBarWrapper>
       </Div>
     </>
   );
@@ -22,3 +42,53 @@ const Div = styled.div`
   overflow: hidden;
   padding: 1rem;
 `;
+
+const NavigationBarWrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+  padding-bottom: 1rem;
+  margin-top: 3rem;
+`;
+
+const Header = styled.div``;
+
+const ContentWrapper = styled.div`
+  flex: 1 1 auto; /* Fill remaining space */
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  margin-top: 1rem;
+  padding-bottom: 3rem;
+`;
+
+const Title = styled.div`
+  color: #003d62;
+  font-size: 1rem;
+  font-weight: 600;
+  margin-bottom: 1rem;
+  margin-left: 1rem;
+`;
+
+const ArticleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const CategoryWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Img = styled.div``;
+
+const CategoryName = styled.div``;
