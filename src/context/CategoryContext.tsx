eb64aab03bdd85ec059fb9e1661626f7d8e21ad0
yujinsim <122004755+nijyoumis@users.noreply.github.com>
2024-11-19 20:31:56 +0900

    category context alert 온보딩 화면에서 제거

--- src/context/CategoryContext.tsx
+++ src/context/CategoryContext.tsx
@@ -22,7 +22,7 @@ interface CategoryProviderProps {
 
 export const CategoryProvider = ({ children }: CategoryProviderProps) => {
   const [selectedCategories, setSelectedCategory] = useState<string[]>([]);
-
+  const location = window.location.pathname;
   //첫 로드 시 - api 호출로 가져오기
   useEffect(() => {
     const loadCategories = async () => {
@@ -35,15 +35,8 @@ export const CategoryProvider = ({ children }: CategoryProviderProps) => {
         alert("아직 카테고리를 설정하지 않았어요. 카테고리를 설정해주세요!");
       }
     };
-    loadCategories();
+    if (location !== "/") loadCategories();
   }, []);
-
-  // edit 으로 바뀌었다면 해당 provider를 사용하는 하위 컴포넌트들에도 반영
-  // .. 은 context가 알아서 해주는거 아닌가?
-  // edit은 어처피 수정페이지만 하니까 거기서 처리 하면 될듯
-  // useEffect(() => {
-  //   setSelectedCategory(data);
-  // }, [selectedCategories]);
 
   const handleCategorySelection = (category: string, isSelected: boolean) => {
     setSelectedCategory((current) =>
